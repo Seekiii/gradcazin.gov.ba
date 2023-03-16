58712f523f3d96ab85c613fa43722c70bd7d3ec2
šeki <64194468+Seekiii@users.noreply.github.com>
2023-03-16 22:52:42 +0100

    ©

---
+++ static/js/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+  $('.dropdown').hover(function() {
+    $(this).find('.dropdown-content').stop(true, true).fadeIn(200);
+  }, function() {$(this).find('.dropdown-content').stop(true, true).fadeOut(200); });
+
+  $("#cr").text(`© ${new Date().getFullYear()} Grad Cazin`)
+  console.log('%cPotreban ti je website?\n--------------------------------------------------------------------\n| Github:                    | Instagram:                          |\n| https://github.com/seekiii | https://instagram.com/ljubijankiic_ |\n--------------------------------------------------------------------', 'color: #ff9600; font-weight: bold;font-size:14px');
+});
+
+ucitani_brojevi = false
+function uzmi_brojeve(id,broj,preskok=1){
+  let counter = 0;
+  const target = broj;
+  const element = document.querySelector(id);
+    const interval = setInterval(() => {
+      counter += preskok;
+      if (id == "#povrsina"){element.innerHTML = String(counter).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.')+"km²";}
+      else{element.innerHTML = String(counter).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');}
+      if (parseInt(counter) >= target) {
+        clearInterval(interval);
+      }
+    }, 10);
+}
+
+$(document).scroll(function() {
+  var num = $(this).scrollTop()
+  if (num > 1000){
+    if (ucitani_brojevi){return}
+    ucitani_brojevi = true
+    uzmi_brojeve("#povrsina",356,2)
+    uzmi_brojeve("#broj_stanovnika",66200,100)
+    uzmi_brojeve("#hektar",21331,50)
+    uzmi_brojeve("#gazinstva",3588,20)
+  }
+});
